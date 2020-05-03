2182570af70450bd416d01e0807e53ff61c3d341
Zaiyr <zaiyr.00@gmail.com>
2020-05-03 22:13:10 +0600

    implemneted jwt token for authorization and put,get,delete,put requests

--- src/main/java/kg/webproject/quiz/config/SwaggerConfig.java
+++ src/main/java/kg/webproject/quiz/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
+
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors
                         .basePackage("kg.webproject.quiz.ui.controllers"))
